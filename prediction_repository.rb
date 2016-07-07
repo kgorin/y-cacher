b5f0f5023d6891957c274233139dc73d31abf14f
kgorin <machineshopflow@gmail.com>
2016-07-07 19:06:08 +0300

    Better defaults handling for prediction repository

--- prediction_repository.rb
+++ prediction_repository.rb
@@ -3,15 +3,29 @@ require './cache'
 require './yandex_predictor_client'
 
 class PredictionRepository
-  def initialize
-    @client = YandexPredictorClient.new
-    @cache = Cache.new
+  def initialize(client = nil, cache = nil)
+    @client = client
+    @cache = cache
+    set_defaults
+  end
+
+  def set_defaults
+    @client ||= YandexPredictorClient.new
+    @cache ||= Cache.new
   end
 
   def get_prediction(query)
     cached = @cache.get(query)
-    return Prediction.new(cached) if cached
+    if cached
+      Prediction.new(cached)
+    else
+      fetched_prediction(query)
+    end
+  end
 
+  private
+
+  def fetched_prediction(query)
     raw_prediction = @client.fetch_prediction(query)
     prediction = Prediction.new(raw_prediction)
 
